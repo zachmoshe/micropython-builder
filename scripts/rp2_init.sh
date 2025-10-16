d24fdaa04a687d6bca90ff6c1fdeb555f17e74bb
zmoshe <zmoshe@nvidia.com>
2025-10-16 08:37:55 +0300

    applying the same MICROPYTHON_TAG change to rp2_init.sh

--- scripts/rp2_init.sh
+++ scripts/rp2_init.sh
@@ -26,7 +26,8 @@ git clone https://github.com/v923z/micropython-ulab ulab || git -C ulab pull
 
 # only check out micropython, if it is not available locally, otherwise, pull
 git clone https://github.com/micropython/micropython micropython || git -C micropython pull
-git -C micropython checkout v1.26.0
+: ${MICROPYTHON_TAG:=$(git tag --sort -v:refname | grep -v preview | head -n 1)}
+git -C micropython checkout ${MICROPYTHON_TAG}
 
 cd micropython
 git submodule update --init lib/tinyusb
