d93d9db8dfdcdb1d42322ed00ba987e576c5e202
zmoshe <zmoshe@nvidia.com>
2025-10-25 20:37:58 +0300

    another fix2

--- scripts/init.sh
+++ scripts/init.sh
@@ -24,7 +24,9 @@ git clone https://github.com/v923z/micropython-ulab ulab || git -C ulab pull
 git clone https://github.com/micropython/micropython micropython || git -C micropython pull
 #: ${MICROPYTHON_TAG:=$(git tag --sort -v:refname | grep -v preview | head -n 1)}
 # git -C micropython checkout ${MICROPYTHON_TAG}
-git tag --sort -v:refname
+echo "TAGS:"
+git tag --sort -v:refnamet -C micropython tag --sort -v:refname
+echo "-------"
 MICROPYTHON_TAG=${MICROPYTHON_TAG:-$(git tag --sort -v:refname | grep -v preview | head -n 1)}
 echo MICROPYTHON_TAG \"${MICROPYTHON_TAG}\"
 
